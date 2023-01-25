5b08a4cf60a9b83fd42624cd664802e93d3129c9
GAMEX <123528470+GAMEX-990@users.noreply.github.com>
2023-01-25 10:52:21 +0700

    Update action.ts

--- action.ts
+++ action.ts
@@ -1,10 +1,11 @@
-
-
+const text ="Hello"
+const textkub = "Prayuth"
 function recursiveReverseString(text: string) : string{
     if (text.length <= 0){ // Base Case
         return ''
     }
     return recursiveReverseString(text.slice(1)) + text.charAt(0)
 }
+console.log(recursiveReverseString('Prayuth'))
 console.log(recursiveReverseString('Hello'))
 
